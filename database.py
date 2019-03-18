cb629838958068556b2565e759704814610ce580
ThomGeG <tgegaskell@gmail.com>
2019-03-18 21:02:16 +1100

    Added db changelogs

--- database.py
+++ database.py
@@ -1,4 +1,7 @@
+import os
 import mysql.connector
+
+CHANGELOG_DIR = "database_scripts"
 
 mydb = mysql.connector.connect(
   host="localhost",
@@ -6,4 +9,8 @@ mydb = mysql.connector.connect(
   passwd="password"
 )
 
-print(mydb)
+db_cursor = mydb.cursor()
+changelogs = [os.path.join(CHANGELOG_DIR, fname) for fname in os.listdir(CHANGELOG_DIR)]
+
+for changelog in changelogs:
+    db_cursor.execute(open(changelog, 'r').read())
